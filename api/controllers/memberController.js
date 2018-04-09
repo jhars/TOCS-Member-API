c7e7a3ed9f31714c8f235927961d7b8c3279f229
Jon Harlan <jharlan@iwerk.com>
2018-04-09 17:35:54 -0400

    Code Clean + Everything should be working

--- api/controllers/memberController.js
+++ api/controllers/memberController.js
@@ -8,7 +8,222 @@ const mongoose = require('mongoose'),
 const bearerToken = "Bearer 4a0443d7ea815fb1404e65e07da04d2af54bd97a3ae33134b6aa8d03ffa79458"
 
 // ============== API METHODS ===================//
+//_________________________________________________________________
+// GET - List All Members
+exports.list_all_members = function(req, res) {
+  Member.find({}, function(err, member) {
+    
+    if (err) {
+      res.status(400); // Bad Request
+      res.send(err)
+    } else {
+      res.json(member)
+    }
+    
+  })
+};
 
+//_________________________________________________________________
+// POST -  .find {Member by:EMAIL}
+exports.find_member_by_email = function(req, res) {
+  Member.find({email: req.body.email}, function(err, member) {
+    
+    console.log(member);
+
+    if (err) {
+      res.status(400); // Bad Request
+      // res.status(401); // Unauthorized
+      // res.status(402); // payment required
+      // res.status(403);    // forbidden
+      res.send(err);      
+    } else if (member.length == 0) {
+      res.status(403);
+      res.json("No TOCS/COBOT Members w/ with matching email");  
+    } else {
+      
+      var obj1;
+      for (var i in member) {
+        const plan = member[i]['membership_plan'];
+
+        if (plan == ('Road Warrior' || 'Small Business Membership')) {
+          // res.json("Access to Premimum WiFi Granted");
+          res.status(200)
+        } else {
+          res.status(401); // Unauthorized
+        }
+
+        console.log(obj1); //need smarter logic for handling membership_plans
+      }
+
+      res.json(member);
+    }
+  
+  });
+};
+
+//_________________________________________________________________
+// POST - .find {Member by:PHONE}
+// ToDo: Normalize Phone String in fetch_all_members
+exports.find_member_by_phone = function(req, res) {
+  Member.find({phone: req.body.phone}, function(err, member) {
+    if (err)
+      res.send(err);
+    res.json(member);
+  });
+};
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+// - - - - - - - - - - COBOT API ROUTES - - - - - - - - - - - - - - - - - - - - //
+
+
+//_________________________________________________________________
+//GET - Subscribe to COBOT
+// =>   Subscription: confirm_membership
+
+exports.subscribe_to_cobot_comfirm_membership_subscription = function(req, res) {
+    console.log(req.body);
+    
+    const callbackRoute = "https://tocs-api.herokuapp.com/member/confirm"
+
+    var cobotSubscribeRequest = {
+      method: 'POST',
+      uri: "https://the-office-ro.cobot.me/api/subscriptions/",
+      headers: {
+        "Authorization": bearerToken
+      },
+      body: {
+        "event": "confirmed_membership",
+        "callback_url": callbackRoute
+      },
+      json: true // Automatically stringifies the body to JSON
+    };
+    
+    rp(cobotSubscribeRequest).then(data => {
+      
+      console.log("---------- COBOT SUBSCRIBE REQUEST RESPONSE DATA: ---------- \n" + data)
+
+    }).catch(err => {
+      console.log("Houston we Have a Problem subscribing to Cobot WEBHOOK 01")
+      console.log(err)
+      res.send(err)
+    })
+  };
+
+
+
+//_________________________________________________________________
+//COBOT sends "POST" request to this ROUTE
+// Receive confirmation from COBOT that a new member has signed up
+// so...
+// Create a New Member in the TOCS API Database
+
+exports.confirm_membership_cobot_subscription = function(req, res) {
+  const memberData = req.body
+  console.log("THIS IS MY MEMBER DATA: ->\n" + memberData);
+
+  const fetchedURL = req.body["url"]
+  console.log("Feteched URL: ->\n" + fetchedURL);
+
+  // https://the-office-ro.cobot.me/api/memberships/dae9fc20daea2df5700de5d6755027d7
+  const getNewMemberCobotAPIRequest = {
+    url: fetchedURL,
+    headers: {
+      "Authorization": bearerToken
+    },
+    json: true
+  }
+  
+  rp(getNewMemberCobotAPIRequest).then(data => {
+
+    console.log("========== Sanity Check #1 ==========")
+
+    let id = data["id"]
+    let name = data["name"]
+    let email =data["email"]
+    let phone = data["phone"]
+    let membership_plan = data["plan"]["name"]
+
+    console.log("========== Sanity Check #2 ==========")
+
+    var member = new Member({
+      name: name,
+      email: email,
+      phone: phone,
+      membership_plan: membership_plan
+    })
+
+    console.log("MEMBER DB OBJECT: " + member)
+    member.save()
+
+    console.log("========== Sanit Check #3 ==========")
+
+    var obj1;
+    for (var property1 in data) {
+      obj1 = data[property1];
+      console.log(obj1);
+      console.log("========== SANITY Check " + property1 + " ==========")
+    }
+
+    
+    console.log("========== Sanit Check #4 ==========")
+
+
+  }).catch(err => {
+      console.log("Houston we Have a Problem subscribing to Cobot WEBHOOK 02")
+      console.log(err)
+    res.send(err)
+  })
+
+
+};
+
+//_______________________________________________________________
+// POST - Route setup for Captive-Portal REST-Auth tool to hit.
+// ...this is the 'gateway' to premium network
+
+// authenticate_member_ironwifi
+exports.authenticate_member_ironwifi = function(req, res) {
+  Member.find({email: req.body.email}, function(err, member) {
+    
+    // console.log(req.params.member_email);
+    // console.log(req.query);
+    // console.log(member);
+
+    if (err) {
+      res.status(400); // Bad Request
+      // res.status(401); // Unauthorized
+      // res.status(402); // payment required
+      // res.status(403);    // forbidden
+      res.send(err);      
+    } else if (member.length == 0) {
+      res.status(403);
+      res.json("No TOCS/COBOT Members w/ with matching email");  
+    } else {
+      
+      var obj1;
+      for (var i in member) {
+
+        const plan = member[i]['membership_plan'];
+
+        if (plan == 'Road Warrior' || plan == 'Small Business Membership') {
+          // res.json("Access to Premimum WiFi Granted");
+          res.status(200)
+        } else {
+          res.status(401); // Unauthorized
+        }
+
+        console.log(obj1); //need smarter logic for handling membership_plans
+      }
+
+      res.json(member);
+    }
+  
+  });
+};
+
+// ######################################################################## //
+// ########################### DO NOT CALL THIS ROUTE ##################### //
+// ############## Unless you wnat to populate the DATABASE ############### //
 
 // GET - Populate 'members' database (via COBOT API)
 exports.fetch_all_members = function(req, res) {
@@ -51,274 +266,11 @@ exports.fetch_all_members = function(req, res) {
   })
 };
 
-//==================================================
-//==================================================
+// ######################################################################## //
+// ######################################################################## //
+// ######################################################################## //
 
 
-exports.subscribe_to_cobot_comfirm_membership_subscription = function(req, res) {
-    
-    console.log(req.body);
-    const callbackRoute = "https://tocs-api.herokuapp.com/member/confirm"
-    // const callbackRoute = "https://localhost:300/member/confirm"
-
-    //"Bearer 912e7a5f4c1cb6f3078f45b584cdf38fd70b6683dd3cc6a075522143bc484af2"
-    // 99d6e5ea15c649d04cc99a563b36db74cd045421ecdb29a4c53f0e8ee9aaee46
-
-    // IS THIS THE ACCCESS TOKEN NOW??
-    // 4a0443d7ea815fb1404e65e07da04d2af54bd97a3ae33134b6aa8d03ffa79458
-
-    var cobotSubscribeRequest = {
-      method: 'POST',
-      uri: "https://the-office-ro.cobot.me/api/subscriptions/",
-      headers: {
-        "Authorization": bearerToken
-      },
-      body: {
-        "event": "confirmed_membership",
-        "callback_url": callbackRoute
-      },
-      json: true // Automatically stringifies the body to JSON
-    };
-    
-    
-    rp(cobotSubscribeRequest).then(data => {
-      console.log("---------- COBOT SUBSCRIBE REQUEST RESPONSE DATA: ---------- \n" + data)
-
-      res.json(member)
-      // res.status(200)
-
-    }).catch(err => {
-      // console.log("Houston we Have a Problem subscribing to Cobot WEBHOOK")
-      // console.log(err)
-      res.send(err)
-    })
-  };
-
-
-//COBOT sends "POST" request to this ROUTE
-
-exports.confirm_membership_cobot_subscription = function(req, res) {
-  const memberData = req.body
-  console.log("THIS IS MY MEMBER DATA: ->\n" + memberData);
-
-  const fetchedURL = req.body["url"]
-    console.log("Feteched URL: ->\n" + fetchedURL);
-
-  // https://the-office-ro.cobot.me/api/memberships/dae9fc20daea2df5700de5d6755027d7
-  const getNewMemberCobotAPIRequest = {
-    url: fetchedURL,
-    headers: {
-      "Authorization": bearerToken
-    },
-    json: true
-  }
-  
-  rp(getNewMemberCobotAPIRequest).then(data => {
-
-    //Create New Member in TOCS-API from a new member being created on COBOT
-    console.log("THEN DATAAAAAA...." + data)
-    console.log("THEN LENGTH...." + data)
-    console.log("Type OF...." + (typeof data));
-    // console.log("Type OF...." + object(data).;
-
-    // console.log(json);
-
-    // let json = JSON.stringify(data);
-    // let json = JSON.parse(data);
-    // console.log(json);
-    console.log("========== Sanity Check #1 ==========")
-    
-    console.log("MEMBER ID: " + data["id"]);
-    console.log("MEMBER EMAIL: " + data["email"]);
-    console.log("MEMBER NAME: " + data["name"]);
-    console.log("MEMBER PHONE: " + data["phone"]);
-
-    let id = data["id"]
-    let name = data["name"]
-    let email =data["email"]
-    let phone = data["phone"]
-    let membership_plan = data["plan"]["name"]
-
-
-
-
-    console.log("========== Sanity Check #2 ==========")
-
-    var member = new Member({
-      name: name,
-      email: email,
-      phone: phone,
-      membership_plan: membership_plan
-    })
-
-    console.log("MEMBER NAME: " + member.name)
-    member.save()
-
-    console.log("========== Sanit Check #3 ==========")
-
-    var obj1;
-    for (var property1 in data) {
-      obj1 = data[property1];
-      console.log(obj1);
-      console.log("========== SANITY Check " + property1 + " ==========")
-    }
-
-    
-    console.log("========== Sanit Check #4 ==========")
-
-    
-    // var member = new Member({
-    //   name: data["name"],
-    //   email: data["email"],
-    //   phone: data["phone"],
-    //   membership_plan: obj1["plan"]["name"]
-    // })
-
-    // console.log("MEMBER NAME: " + member.name)
-    // member.save()
-
-
-    // res.json(member)
-
-    // Member.find({email: member.email}, function(err, member) {
-    //   console.log("MEMBER DB OBJECT: " + member)
-
-    //   if (err) {
-    //     // res.status(400); // Bad Request
-    //     // res.status(401); // Unauthorized
-    //     // res.status(402); // payment required
-    //     // res.status(403);    // forbidden
-    //     res.send(err);     
-    //   }
-    // })
-
-
-  }).catch(err => {
-      // console.log("Houston we Have a Problem subscribing to Cobot WEBHOOK")
-      // console.log(err)
-    res.send(err)
-  })
-
-
-};
-
-// authenticate_member_ironwifi
-exports.authenticate_member_ironwifi = function(req, res) {
-  Member.find({email: req.body.email}, function(err, member) {
-    
-    // console.log(req.params.member_email);
-    // console.log(req.query);
-    // console.log(member);
-
-    if (err) {
-      res.status(400); // Bad Request
-      // res.status(401); // Unauthorized
-      // res.status(402); // payment required
-      // res.status(403);    // forbidden
-      res.send(err);      
-    } else if (member.length == 0) {
-      res.status(403);
-      res.json("No TOCS/COBOT Members w/ with matching email");  
-    } else {
-
-      
-
-
-
-      var obj1;
-
-      for (var i in member) {
-
-        const plan = member[i]['membership_plan'];
-
-        if (plan == 'Road Warrior' || plan == 'Small Business Membership') {
-          
-          res.status(200)
-          // res.json("Access to Premimum WiFi Granted");
-
-        } else {
-          res.status(401); // Unauthorized
-        }
-
-        console.log(obj1); //need smarter logic for handling membership_plans
-      }
-
-      res.json(member);
-    }
-  
-  });
-};
-
-// POST -  .find {Member by:EMAIL}
-exports.find_member_by_email = function(req, res) {
-  Member.find({email: req.body.email}, function(err, member) {
-    
-    console.log(member);
-
-    if (err) {
-      res.status(400); // Bad Request
-      // res.status(401); // Unauthorized
-      // res.status(402); // payment required
-      // res.status(403);    // forbidden
-      res.send(err);      
-    } else if (member.length == 0) {
-      res.status(403);
-      res.json("No TOCS/COBOT Members w/ with matching email");  
-    } else {
-
-      
-
-
-
-      var obj1;
-
-      for (var i in member) {
-
-        const plan = member[i]['membership_plan'];
-
-        if (plan == ('Road Warrior' || 'Small Business Membership')) {
-          
-          res.status(200)
-          // res.json("Access to Premimum WiFi Granted");
-
-        } else {
-          res.status(401); // Unauthorized
-        }
-
-        console.log(obj1); //need smarter logic for handling membership_plans
-      }
-
-      res.json(member);
-    }
-  
-  });
-};
-
-// POST - .find {Member by:PHONE}
-// ToDo: Normalize Phone String in fetch_all_members
-exports.find_member_by_phone = function(req, res) {
-  Member.find({phone: req.body.phone}, function(err, member) {
-    if (err)
-      res.send(err);
-    res.json(member);
-  });
-};
-
-
-
-// GET - List All Members
-exports.list_all_members = function(req, res) {
-  Member.find({}, function(err, member) {
-    
-    if (err) {
-      res.status(400); // Bad Request
-      res.send(err)
-    } else {
-      res.json(member)
-    }
-    
-  })
-}
 
 //==================================================
 //==================================================
